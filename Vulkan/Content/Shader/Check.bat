a97c5041a9d93d9df549437d49d786844ba63a5b
fdcumt <1040183251@qq.com>
2025-06-03 14:21:36 +0800

    修改

---
+++ Vulkan/Content/Shader/Check.bat
@@ -0,0 +1,9 @@
+
+
+D:\SDK\VulkanSDK\Bin\spirv-val.exe ./Spv/VertexShader.spv
+D:\SDK\VulkanSDK\Bin\spirv-val.exe ./Spv/FragmentShader.spv
+::D:\SDK\VulkanSDK\Bin\glslc.exe FragmentShader.frag -o FragmentShader.spv
+
+
+pause
+
--- Vulkan/Content/Shader/Spv/Check.bat
+++
@@ -1,8 +0,0 @@
-
-
-D:\SDK\VulkanSDK\Bin\spirv-val.exe VertexShader.spv
-::D:\SDK\VulkanSDK\Bin\glslc.exe FragmentShader.frag -o FragmentShader.spv
-
-
-pause
-
